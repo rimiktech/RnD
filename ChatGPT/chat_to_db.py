361554017f27c5abcdb98787f1a667702b01db02
fezul123 <fezul@rimiktech.com>
2024-07-03 17:47:22 +0530

    chat to db without langchain

--- ChatGPT/chat_to_db.py
+++ ChatGPT/chat_to_db.py
@@ -9,30 +9,50 @@ from langchain_core.runnables import RunnablePassthrough
 load_dotenv()
 mysql_uri = os.getenv("mysql_uri")
 
-template = """Based on the table schema below, write a SQL query that would answer the user's question:
-{schema}
-
-Question: {question}
-SQL Query:"""
-
-prompt = ChatPromptTemplate.from_template(template)
-
 print("Connecting to database...")
 db = SQLDatabase.from_uri(mysql_uri)
 print("Connected.")
 
-def get_schema(input):
-    schema = input['database'].get_table_info()
-    return schema
-
 llm = ChatOpenAI()
 
-sql_chain = (
-    RunnablePassthrough.assign(schema=get_schema)
-    | prompt
-    | llm.bind(stop=["\nSQLResult:"])
-    | StrOutputParser()
-)
+def get_schema(db):
+    schema = db.get_table_info()
+    return schema
+
+schema = get_schema(db)
+
+print(schema)
+exit()
+user_question = 'How many users are there?'
+
+template = """You are a SQLite expert. Given an input question, first create a syntactically correct SQLite query to run, then look at the results of the query and return the answer to the input question.
+Unless the user specifies in the question a specific number of examples to obtain, query for at most 5 results using the LIMIT clause as per SQLite. You can order the results to return the most informative data in the database.
+Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in double quotes (") to denote them as delimited identifiers.
+Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
+Pay attention to use date('now') function to get the current date, if the question involves "today".
+
+Use the following format:
+
+Question: Question here
+SQLQuery: SQL Query to run
+SQLResult: Result of the SQLQuery
+Answer: Final answer here
+
+Only use the following tables:
+{0}
+
+Question: {1}"""
+
+prompt = template.format(schema, user_question)
+
+import pdb
+pdb.set_trace()
+
+
+
+
+print("hhh")
+sql_chain = (RunnablePassthrough.assign(schema=get_schema) | prompt | llm.bind(stop=["\nSQLResult:"]) | StrOutputParser())
 
 print("Creating SQL query...")
 user_question = 'How many users are there?'
