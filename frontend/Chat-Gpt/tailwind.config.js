2afcab59a266c41d8c75b518b1c6b54f84fa987e
Vivek Singh <vivek@rimiktech.com>
2024-07-05 10:53:37 +0530

    Create frontend and establish connectivity

---
+++ frontend/Chat-Gpt/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", 'node_modules/flowbite-react/lib/esm/**/*.js'],
+  theme: {
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
