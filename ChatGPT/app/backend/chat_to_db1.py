61e53c0abe65ae0d97e3606e1deb41afb562d665
fezul123 <fezul@rimiktech.com>
2024-07-08 13:27:36 +0530

    changes

--- ChatGPT/app/backend/chat_to_db1.py
+++ ChatGPT/app/backend/chat_to_db1.py
@@ -30,7 +30,7 @@ Unless the user specifies in the question a specific number of examples to obtai
 Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in double quotes (") to denote them as delimited identifiers.
 Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
 Pay attention to use date('now') function to get the current date, if the question involves "today". Please do not use double quotation marks (") for table names and column names when writing SQL queries. For example, instead of writing SELECT * FROM "user";, write SELECT * FROM user;.
-Please refrain from using backslashes (\) also while writing queries.
+Please refrain from using backslashes (\\) also while writing queries.
 
 Question: 
 {0}
