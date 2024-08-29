5fc3041b84e2df582ba08146ef98aa7fcc57b899
Awdesh Kumar <awdesh@rimitech.com>
2024-08-29 19:36:05 +0530

    modify in airbnb script

--- automation_scraping/airbnb_automation.py
+++ automation_scraping/airbnb_automation.py
@@ -1,4 +1,3 @@
-
 import os
 import time
 import dateparser
@@ -13,18 +12,8 @@ from airtable import Airtable
 AIRTABLE_BASE_ID = 'apprRoN4AnjIokL4p'
 AIRTABLE_API_KEY = 'patQRtHf3kXuLceyW.4a5362f890bdc06343390ced8d1c62772460a82ff0f65b0986273c7ad88e0643'
 AIRTABLE_TABLE_NAME = 'Airbnb Reviews'
-
-
-#TEST ACCOUNT
-# AIRTABLE_BASE_ID = 'appnjCm9aFRId8lxr'
-# AIRTABLE_API_KEY = 'patiiPih4ERBsP9qI.2b9c1d9df084f42a04fa6c311a738ba8765a22a9591b14fc5a6114b960bcdd5a'
-# AIRTABLE_TABLE_NAME = 'Review'
-
-
-
-# Initialize Airtable
+AIRTABLE_TABLE_LISTINGS="Listings"
 airtable = Airtable(AIRTABLE_BASE_ID, AIRTABLE_TABLE_NAME, api_key=AIRTABLE_API_KEY)
-
 
 logs_path = "./"
 def log(message):
@@ -39,28 +28,45 @@ def log(message):
             file.write(str(message) + "\n")
     except Exception as err:
         print(err)
+def get_all_listing():
+    airtable1 = Airtable(AIRTABLE_BASE_ID, AIRTABLE_TABLE_LISTINGS, api_key=AIRTABLE_API_KEY)
+    records = airtable1.get_all(fields=['Listing ID', 'Listing URLS'])
+    listing_urls = [record['fields']['Listing URLS'] for record in records if 'Listing URLS' in record['fields']]
+    listing_ids = [record['fields']['Listing ID'] for record in records if 'Listing ID' in record['fields']]
+    print(f"------------:{len(listing_ids)}")
+    return listing_urls,listing_ids
 
-def check_duplicate(listing_id, review_date):
-    """Check if the record with the given listing_id and review_date already exists in Airtable."""
-    log(f"Checking for duplicate entry for Listing ID: {listing_id}, Review Date: {review_date}.")
+def update_listing(listing_id, total_review_found):
     try:
-        formula = f"AND({{Listing ID}} = '{listing_id}', {{Review Date}} = '{review_date}')"
-        records = airtable.get_all(formula=formula)
-        return len(records) > 0
+        log(f"-------------------{listing_id,total_review_found}")
+        airtable1 = Airtable(AIRTABLE_BASE_ID, AIRTABLE_TABLE_LISTINGS, api_key=AIRTABLE_API_KEY)
+        records = airtable1.get_all(formula=f"{{Listing ID}} = '{listing_id}'")  
+        if records:
+            record_id = records[0]['id']  # Get the Airtable record ID
+            update_data = {
+                "No of Review": total_review_found
+            }
+            airtable1.update(record_id, update_data)
+            log(f"Updated the 'No of Review' in the listing table for Listing ID: {listing_id}")
+        else:
+            log(f"No record found with Listing ID: {listing_id}")    
     except Exception as e:
-        log(f"Error checking for duplicate: {e}")
-        return False
+        log(f"Failed to update the 'No of Review' for Listing ID: {listing_id}. Error: {e}")
 
-def save_to_airtable(data):
+
+def save_to_airtable(data_batch):
     try:
-        airtable.insert(data)
-        log("Data saved to Airtable successfully!")
+       
+        airtable.batch_insert(data_batch, typecast=True)
+        log("Data batch saved to Airtable successfully!")
     except Exception as e:
         log(f"Failed to save data to Airtable: {e}")
+
 
 def load_all_listings(driver):
     while True:
         try:
+            log(f"Loading listing....")
             show_more_button = driver.find_element(By.CSS_SELECTOR, 'div.lk34ed1 button')
             driver.execute_script("arguments[0].scrollIntoView();", show_more_button)
             show_more_button.click()
@@ -69,58 +75,66 @@ def load_all_listings(driver):
             log(f"No more listings to load or error occurred:{e}")
             break
 
-def main(url):
+def main():
     try:
-        total_review=0
+     
+
+        all_listing, listing_ids = get_all_listing()
+        data_batch = []  # List to hold batch data
         chrome_options = webdriver.ChromeOptions()
-        #chrome_options.add_argument("--headless")  # Headless mode
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")  # Disable GPU acceleration
-        chrome_options.add_argument("--no-sandbox")  # Disable sandboxing (useful for headless mode)
+        chrome_options.add_argument("--no-sandbox")  # Disable sandboxing
         chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
-        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")        
+        chrome_options.add_argument(
+            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        )
         driver = webdriver.Chrome(options=chrome_options)
-        driver.get(url)
-        time.sleep(5)
-        button = driver.find_element(By.CSS_SELECTOR, 'div.v1ubu2ue button')
-        button.click() 
-        time.sleep(5)           
-        load_all_listings(driver)
-        source_page = driver.page_source
-        soup = BeautifulSoup(source_page, 'html.parser')        
-        divs = soup.find('div', class_='gm62wvj')
-        all_listing = divs.find_all('a')
-        log(f"Total listed title: {len(all_listing)}")        
-        for item in all_listing:
-            review_link = item.get('href')
-            listing_id = review_link.split('/')[2].split('?')[0]
-            log(f"Review listed id: {listing_id}")
-            driver.get(f"https://www.airbnb.ae/{review_link}") 
-            time.sleep(2)   
+
+        for i, (url, listing_id) in enumerate(zip(all_listing, listing_ids)):
+            # if i == 5:
+            #     break
+       
+            url = f"https://www.airbnb.ae/{url}"
+            driver.get(url)
+            time.sleep(3)
+            source_page = driver.page_source
+            soup = BeautifulSoup(source_page, 'html.parser')
+
             try:
-                list_title = driver.find_element(By.CLASS_NAME, "_1xxgv6l").text           
+                list_title = driver.find_element(By.CLASS_NAME, "_1xxgv6l").text
                 div_element = driver.find_element(By.CSS_SELECTOR, 'div.rk4wssy')
                 a_tag = div_element.find_element(By.TAG_NAME, 'a')
+
                 if a_tag:
                     a_tag.click()
+                    time.sleep()
                     review_url = driver.current_url
                     log(f"Review url for review: {review_url}")
-                    source_page = driver.page_source            
+                    source_page = driver.page_source
                     soup = BeautifulSoup(source_page, 'html.parser')
                     divs1 = soup.find_all('div', class_='r1are2x1')
-                    total_review=total_review+1
-                    log(f"Total review found:{len(divs1)}")                    
+                    log(f"Total review found: {len(divs1)}")
+
                     for review in divs1:
-                        reviewer_name = review.find('h2').text                       
-                        reviewer_comment = review.find('span', class_="lrl13de atm_kd_19r6f69_24z95b atm_kd_19r6f69_1xbvphn_1oszvuo dir dir-ltr").text
-                        review_rating = review.find('div', class_="c5dn5hn atm_9s_1txwivl atm_cx_t94yts dir dir-ltr").text
-                        review_date = review.find(class_="s78n3tv atm_c8_1w0928g atm_g3_1dd5bz5 atm_cs_10d11i2 atm_9s_1txwivl atm_h_1h6ojuz dir dir-ltr").text.split("·")[1].strip()
+                        reviewer_name = review.find('h2').text
+                        reviewer_comment = review.find(
+                            'span', class_="lrl13de atm_kd_19r6f69_24z95b atm_kd_19r6f69_1xbvphn_1oszvuo dir dir-ltr"
+                        ).text
+                        review_rating = review.find(
+                            'div', class_="c5dn5hn atm_9s_1txwivl atm_cx_t94yts dir dir-ltr"
+                        ).text
+                        review_date = review.find(
+                            class_="s78n3tv atm_c8_1w0928g atm_g3_1dd5bz5 atm_cs_10d11i2 atm_9s_1txwivl atm_h_1h6ojuz dir dir-ltr"
+                        ).text.split("·")[1].strip()
                         review_date = review_date.rstrip(',')
                         parsed_date = dateparser.parse(review_date)
-                        formatted_date = parsed_date.strftime("%B %Y") if parsed_date else "Invalid Date"                       
-                        review_location = review.find(class_="s15w4qkt atm_c8_1w0928g atm_g3_1dd5bz5 atm_cs_6adqpa atm_7l_1wzk1hz dir dir-ltr").text
-                        if check_duplicate(listing_id, review_date):
-                            log(f"Duplicate found for Listing ID: {listing_id}, Review Date: {review_date}. Skipping...")
-                            continue
+                        formatted_date = parsed_date.strftime("%B %Y") if parsed_date else "Invalid Date"
+                        review_location = review.find(
+                            class_="s15w4qkt atm_c8_1w0928g atm_g3_1dd5bz5 atm_cs_6adqpa atm_7l_1wzk1hz dir dir-ltr"
+                        ).text
+                        #total_review_found = review.find("div", class_="_s9zd43").text
+
                         data = {
                             "Listing ID": listing_id,
                             "Listing Title": list_title,
@@ -129,19 +143,28 @@ def main(url):
                             "Review Date": formatted_date,
                             "Review Comments": reviewer_comment,
                             "Review Rating": review_rating,
-                            "Review URL": review_url
-                        }                       
-                        save_to_airtable(data)
+                            "Review URL": review_url,
+                        }
+
+                        data_batch.append(data)
+                       
+                        # Insert batch every 10 records
+                        
+                    save_to_airtable(data_batch)
+                    data_batch.clear()     
+                    update_listing(listing_id, len(divs1))
                 else:
-                    log(f"No review not found for :{review_link}")
-                    continue 
+                    log(f"No review found fo ---------------r: {url}")
+                    continue
             except Exception as ee:
-                log(f"No review found for: {review_link}")
-                continue       
-        log(f"Total reviews found for Listed Title:{total_review}")
+                print(ee)
+                #update_listing(listing_id, "No Review Found")
+                log(f"No review found for: {url}")
+                continue
+
     except Exception as e:
-        log(f"An error occurred: {e}")            
+        log(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    url = "https://www.airbnb.ae/users/show/74933177"
-    main(url)
+    #url = "https://www.airbnb.ae/users/show/74933177"
+    main()
